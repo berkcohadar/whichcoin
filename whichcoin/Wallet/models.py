aa722568bccf136d586e4fa43693017ca8d424ad
emre-serdar <eserdar1@binghamton.edu>
2021-06-26 23:07:18 +0300

    Public key is created with SHA256

--- whichcoin/Wallet/models.py
+++ whichcoin/Wallet/models.py
@@ -1,33 +1,57 @@
 from django.db import models
 from django.contrib.auth.models import User
 from Market.models import CurrencyMarket
+import re
+import hashlib
+import binascii
+import base64
+
 # Create your models here.
 # import random
 # hash = random.getrandbits(256)
 
+
 class Wallet(models.Model):
     name = models.CharField(max_length=31)
-    user = models.ForeignKey(User,on_delete=models.CASCADE,null=False,blank=False)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=False, blank=False)
     public_key = models.CharField(max_length=255)
-    
+
     def generatePublicKey(self):
         # SHA256 ile public key olusturulacak.
-        return "whichCoin-PublicKeyGenerator"
-    
-    def save(self,*args,**kwargs):
+        self.name = self.name.strip()
+
+        if (re.search(r'^ssh-(?:rsa|dss) ', self.name)):
+            self.name = self.name.split(None, 2)[1]
+
+        try:
+            self.name = bytes(self.name, 'ascii')
+        except TypeError:
+            self.name = bytes(self.name)
+
+        digest = hashlib.sha256(binascii.a2b_base64(self.name)).digest()
+        encoded = base64.b64encode(digest).rstrip(
+            b'=')  # ssh-keygen strips this
+
+        return encoded
+
+    def save(self, *args, **kwargs):
         if self.public_key == "":
             self.public_key = self.generatePublicKey()
 
-        return super(Wallet,self).save(*args,**kwargs)
-    
+        return super(Wallet, self).save(*args, **kwargs)
+
     def __str__(self):
         return f"{self.user.__str__()}'s {self.name} wallet"
 
 
 class WalletItem(models.Model):
-    wallet_id = models.ForeignKey(Wallet,on_delete=models.CASCADE,null=False,blank=False)
-    currencyMarket_id = models.ForeignKey(CurrencyMarket,on_delete=models.CASCADE,null=False,blank=False)
-    data_created = models.DateTimeField(verbose_name='wallet created',auto_now_add=True)
+    wallet_id = models.ForeignKey(
+        Wallet, on_delete=models.CASCADE, null=False, blank=False)
+    currencyMarket_id = models.ForeignKey(
+        CurrencyMarket, on_delete=models.CASCADE, null=False, blank=False)
+    data_created = models.DateTimeField(
+        verbose_name='wallet created', auto_now_add=True)
     first_price = models.FloatField()
     current_PNL = models.FloatField()
 
